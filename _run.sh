2a9b775f54505f81f96df7cb97bdc824a7d29e17
Matt Curfman <matt@tecnovelty.com>
2024-09-09 21:29:01 -0700

    Add quant suppport

--- _run.sh
+++ _run.sh
@@ -5,13 +5,14 @@ source /root/miniforge3/bin/activate ov_genai
 huggingface-cli login --token $HUGGINGFACE_TOKEN
 
 download_model(){
-  if [ ! -d "/models/$1" ]; then
-    optimum-cli export openvino --trust-remote-code --model $1 /models/$1
+  if [ ! -d "/models/$1/$2" ]; then
+    optimum-cli export openvino --weight-format $2 --model $1 /models/$1/$2
   fi
 }
 
 run_benchmark(){
-  python benchmark.py -m /models/$1 -p "What is the meaning of life?" -n 2 -d $2 --torch_compile_backend openvino
+  echo 'Running benchmark ' $1 ' ' $2 ' ' $3
+  python benchmark.py -m /models/$1/$2 -p "What is the meaning of life?" -n 2 -d $3 --torch_compile_backend openvino
 }
 
 _DEVICE=CPU
@@ -21,12 +22,16 @@ fi
 
 # meta-llama/Meta-Llama-3-8B
 # meta-llama/Meta-Llama-3.1-8B
- 
 for model in \
   'TinyLlama/TinyLlama-1.1B-Chat-v1.0' \
   'meta-llama/Llama-2-7b-chat-hf' 
 do
-  download_model $model
-  run_benchmark $model $_DEVICE
+  for quant in \
+    'int8' \
+    'int4'
+  do
+    download_model $model $quant
+    run_benchmark $model $quant $_DEVICE
+  done
 done
 
